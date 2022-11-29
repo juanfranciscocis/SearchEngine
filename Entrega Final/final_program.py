a3661e382c3b5f21717276ec261eadb62e47f442
Juan Francisco Cisneros <juanfrancistm2011@icloud.com>
2022-11-28 19:41:20 -0500

    FINALIZADO PROGRAMA FINAL, FALTA PONER AL ALGORITMO

--- Entrega Final/final_program.py
+++ Entrega Final/final_program.py
@@ -9,6 +9,45 @@ def data():
 
 	return data
 
+
+
+# BOYER MOORE ALGORITHM
+NO_OF_CHARS = 256
+
+def badCharHeuristic(string, size):
+	badChar = [-1]*NO_OF_CHARS
+
+	for i in range(size):
+		badChar[ord(string[i])] = i;
+
+	return badChar
+
+def boyer_moore_search(txt, pat):
+
+	patternFound = []
+
+	m = len(pat)
+	n = len(txt)
+
+	badChar = badCharHeuristic(pat, m)
+
+	s = 0
+	while(s <= n-m):
+		j = m-1
+
+		while j>=0 and pat[j] == txt[s+j]:
+			j -= 1
+
+		if j<0:
+			#print("Pattern occur at shift = {}".format(s))
+			patternFound.append(s)
+			s += (m-badChar[ord(txt[s+m])] if s+m<n else 1)
+		else:
+			s += max(1, j-badChar[ord(txt[s+j])])
+
+
+	return patternFound
+
 # import all functions from the tkinter
 import tkinter as tk
 from tkinter.font import Font
@@ -25,19 +64,26 @@ class Pad(tk.Frame):
 		self.toolbar = tk.Frame(self, bg="#eee")
 		self.toolbar.pack(side="top", fill="x")
 
-		# this will add Highlight button in the window
-		self.bold_btn = tk.Button(self.toolbar, text="Highlight",
+		# create a label widget
+		self.label = tk.Label(self.toolbar, text="Search:")
+		self.label.pack(side="left")
+
+		# create a text entry box
+		self.search_txt = tk.Text(self.toolbar, height=1, width=30)
+		self.search_txt.pack(side="left")
+
+		#this will add Clear button in the window
+		self.search_btn = tk.Button(self.toolbar, text="Search",
 								command=self.highlight_text)
-		self.bold_btn.pack(side="left")
-
-		# this will add Clear button in the window
-		self.clear_btn = tk.Button(self.toolbar, text="Clear",
-								command=self.clear)
-		self.clear_btn.pack(side="left")
+		self.search_btn.pack(side="left")
 
 
 
-		# adding the text
+
+
+
+
+		# adding the text from the text file
 		self.text = tk.Text(self)
 		self.text.insert("end", data())
 		self.text.focus()
@@ -49,11 +95,26 @@ class Pad(tk.Frame):
 	# method to highlight the selected text
 	def highlight_text(self):
 
-		# if no text is selected then tk.TclError exception occurs
-		try:
-			self.text.tag_add("start", "sel.first", "sel.last")
-		except tk.TclError:
-			pass
+		# clear the previous highlighted text
+		self.clear()
+
+		# get the text from the text widget
+		wordToSearch = self.search_txt.get("1.0", "end-1c")
+		wordToSearchLen = len(wordToSearch)
+
+		# search for the text in the text widget
+		founded = boyer_moore_search(data(), str(wordToSearch))
+
+		#show the occurrences of the word
+		self.label = tk.Label(self.toolbar, text= str(len(founded)) + " ocurrences found")
+		self.label.pack(side="right")
+
+		# if found, highlight the text
+		if founded:
+			for i in founded:
+				self.text.tag_add("start", "1.0 + {} chars".format(i), "1.0 + {} chars".format(i + wordToSearchLen))
+		else:
+			print("Pattern not found")
 
 	# method to clear all contents from text widget.
 	def clear(self):
